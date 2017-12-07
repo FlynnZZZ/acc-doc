8f995ba1d6638c1e98645775c2c2f3324807ecca
1510 <1510973267@qq.com>
2017-12-07 17:02:06 +0800

    2017年12月7日 17:02:04

---
+++ Windows/config.js
@@ -0,0 +1,47 @@
+系统配置: 
+  hosts:  
+    todo ✔ 
+  联网控制: 
+Desktop: 桌面 
+  回收站                           
+  Tmp: 临时存放文件夹    ✔               
+    Record: 记录临时存放  ✔          
+    Save: 临时存放      ✔            
+    tmp.rec: 临时记录文件  ✔         
+  应用快捷方式文件夹的快捷方式 ✔       
+  tmp.rec 的快捷方式               
+C: 系统盘 
+  个人帐号 
+    部分程序 
+      atom               
+        packages   ✔               
+D: 应用安装盘 
+  PS: 按类别放置 
+  game                            
+  green                            
+  tls                              
+  web_tls                          
+  media: 媒体相关                  
+  360                              
+  tencent                          
+  应用快捷方式文件夹  ✔               
+E: 文件/项目盘 
+  Accum: 总结积累  ✔       
+    JustMySelf 
+      Private 
+        Diary  
+  Assets: 静态文件存放  ✔            
+  pjct: 个人项目   ✔                 
+  project: 项目   ✔                 
+F: 备份文件/静态资源  ✔              
+应用程序: 
+  QQ输入法: 可帐号同步 
+    自定义短语   ✔ 
+  Chrome: 可帐号同步 
+    书签   ✔  
+  360 极速浏览器: 可帐号同步 
+    书签  ✔ 
+
+
+
+
