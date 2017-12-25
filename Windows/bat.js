5d9a2af006a1889fed76333e22ea30adf45b2ecc
1510 <1510973267@qq.com>
2017-12-25 09:30:34 +0800

    2017年12月25日 09:30:32

--- Windows/bat.js
+++ Windows/bat.js
@@ -1,5 +1,6 @@
 '.bat'批处理脚本 
-  REM 和 ::  注释 
+  ::   单行注释 
+  REM  注释 
   
 隐藏文件夹的'.bat'
   // cls
@@ -38,7 +39,11 @@
   // echo Private created successfully
   // goto End
   // :End
-  
-  
+批量删除文件名称中的文字
+  新建一个文本文档，复制下面内容并保存为.bat文件,运行即可。
+    @echo off& setlocal enabledelayedexpansion
+      for /f "delims=" %%1 in ('dir /a /b') do (set wind=%%1
+      ren "%%~1" "!wind:xxx=!")
+    注：将上面的  “XXX” 改为你需要批量删除的文字。比如我要删除“视频教程”，就将“XXX”修改为“视频教程”
   
   
